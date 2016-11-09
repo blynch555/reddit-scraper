613d259cc400563043127f284c870140980a402d
cynicalfox47 <cynicalfox47@users.noreply.github.com>
2016-11-09 15:07:05 +0800

    Completely overhauled the initialisation script

--- init.php
+++ init.php
@@ -1,7 +1,42 @@
 <?php 
-	require_once('includes/reddit.php');
 
-	$reddit = new reddit;
+/**-------------------------------------------------------------------------**/
+/* CONFIGURE YOUR LISITNGS BELOW */	
 
-	$posts = $reddit->get_posts($_GET['sub']??'NaturePics', $_GET['lim']??5,$_GET['override']??null);
-	$cacheAge = $reddit->getCacheAge($_GET['sub']??'NaturePics');
+//SET TIMEZONE
+$settings['timezone'] = "GMT+8";
+
+//GENERAL CONFIGS
+$settings['subreddit'] = $_GET['sub'] ?? "NaturePics";
+$settings['limit'] = $_GET['lim'] ?? 5;
+$settings['override'] = $_GET['override'] ?? null;
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+/**-------------------------------------------------------------------------**/
+/** INITIAL CHECKS, DON'T ALTER THE CODE UNLESS YOU KNOW WHAT YOU'RE DOING! **/
+/**-------------------------------------------------------------------------**/
+
+//CHECK WHETHER THE CORE FILES EXISTS
+if(!file_exists('includes/reddit.php')) {
+	die("Please check that all the files are in the correct root directory!");
+}
+
+//CHECK WHETER THE CACHE FOLDER EXISTS, IF NOT CREATE IT
+if (!file_exists("cache")&&!is_dir("cache")) {
+	mkdir("cache");
+	header("Location: //".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']);
+}
+
+//LOAD FUNCTIONS FILE
+require_once('includes/reddit.php');
+
+//SET SETTINGS
+date_default_timezone_set($timezone);
+
+//DEFINE NEW VARIABLE
+$reddit = new reddit;
+
+//SUBREDDIT TO DISPLAY
+$posts = $reddit->get_posts($settings['subreddit'],$settings['limit'],$settings['override']);
+$cacheAge = $reddit->getCacheAge($settings['subreddit']);
